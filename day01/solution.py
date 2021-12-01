81becdf8355a7fa66410e591e411f0ce59f76569
Steven van de Graaf <8904453+sgraaf@users.noreply.github.com>
2021-12-01 07:54:38 +0100

    Update to Day 1: Sonar Sweep

--- day01/solution.py
+++ day01/solution.py
@@ -9,28 +9,12 @@ data = to_numbers(read("input.txt"))
 
 # part one
 print("--- Part One ---")
-increased_count = 0
-decreased_count = 0
-for i, measurement in enumerate(data):
-    if i == 0:
-        continue
-    if measurement > data[i - 1]:
-        increased_count += 1
-    elif measurement < data[i - 1]:
-        decreased_count += 1
-
-print(f"There are {increased_count} measurements larger than the previous measurement.")
+print(
+    f"There are {sum(measurement > data[i - 1] for i, measurement in enumerate(data))} measurements larger than the previous measurement."
+)
 
 # part two
 print("--- Part Two ---")
-increased_count = 0
-decreased_count = 0
-for i in range(3, len(data)):
-    sliding_window_sum = sum(data[i - 2 : i + 1])
-    previous_sliding_window_sum = sum(data[i - 3 : i])
-    if sliding_window_sum > previous_sliding_window_sum:
-        increased_count += 1
-    elif sliding_window_sum < previous_sliding_window_sum:
-        decreased_count += 1
-
-print(f"There are {increased_count} sums larger than the previous sum.")
+print(
+    f"There are {sum(sum(data[i - 2 : i + 1]) > sum(data[i - 3 : i]) for i in range(3, len(data)))} sums larger than the previous sum."
+)
